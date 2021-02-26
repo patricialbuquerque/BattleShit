fbf60cc013d803d0350f55769bd8b182ffbbcaff
tandrecrosa14 <79652331+tandrecrosa14@users.noreply.github.com>
2021-02-26 22:14:06 +0000

    enum added and beggining of BattleField

--- src/client/BattleField.java
+++ src/client/BattleField.java
@@ -1,4 +1,14 @@
 package client;
 
 public abstract class BattleField {
+
+    //PROPERTIES
+
+    public static int rows = 10;
+    public static int cols = 10;
+    public static String[][] grid
+    private
+
+
+    //METHODS
 }
--- src/client/Ships.java
+++ src/client/Ships.java
@@ -1,4 +1,34 @@
 package client;
 
 public class Ships {
+
+    public enum ShipType {
+        BATTLESHIP,
+        CRUISER,
+        FRIGATE,
+        MINESWEEPER
+    }
+    private ShipType type;
+    private int size;
+
+
+    public Ships(ShipType type) {
+        this.type = type;
+
+        switch(type) {
+            case BATTLESHIP:
+                size = 5;
+                break;
+            case CRUISER:
+                size = 4;
+                break;
+            case FRIGATE:
+                size = 3;
+                break;
+            case MINESWEEPER:
+                size = 2;
+                break;
+        }
+    }
+
 }
