b31abda5bdb2b3f554047a01a84f4dacadcc51f8
tandrecrosa14 <79652331+tandrecrosa14@users.noreply.github.com>
2021-02-27 17:21:58 +0000

    battlefield superclass added

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,14 +1,34 @@
 package client;
 
 import java.util.HashMap;
+import java.util.TreeMap;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        /*String[][] axis = new String[11][11];
 
-        MenuClient newmenu = new MenuClient();
-        newmenu.mainMenu();
+        System.out.print(" ");
+        for(int i = 0; i < 11; i++)
+            System.out.print(i + " ");
+        System.out.println();
+
+        for(int i = 0; i < 11; i++) {
+            for(int j = 0; j < 11; j++) {
+                axis[i][j] = "";
+
+                if (j == 0) {
+                    System.out.print(i + "|");
+                } else if (j == axis.length - 1) {
+                    System.out.println(i);
+                }else {
+                    axis[i][j] = grid.get(alphabet.substring(j, j + 1) + i);
+                    System.out.print(" " + axis[i][j] + " ");
+                }
+            }
+        }
+         */
 
 
        /* HashMap <String, String> grid = new HashMap<>();
--- src/client/MyClient.java
+++ src/client/MyClient.java
@@ -2,4 +2,5 @@ package client;
 
 public class MyClient extends BattleField {
 
+
 }
