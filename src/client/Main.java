18367ac5eb59e96bb0cb2e97c78c2ff261a8d941
Emanuel_Braga <emanuel.neves.rodrigues@gmail.com>
2021-02-27 15:44:21 +0000

    Estruturado o acesso do client ao server

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,29 +1,12 @@
 package client;
 
-import java.util.HashMap;
-
 public class Main {
 
     public static void main(String[] args) {
 
-
-        MenuClient newmenu = new MenuClient();
-        newmenu.mainMenu();
+        new Game();
 
 
-       /* HashMap <String, String> grid = new HashMap<>();
 
-        String alphabet = "ABCDEFGHIJ";
-
-        for(int i = 0; i < 10; i++){
-            for (int j = 0; j < 10; j++) {
-
-                grid.put(alphabet.substring(i , i + 1) + j, "≈");
-
-            }
-        }
-        for (String j : grid.keySet()) {
-            System.out.println(j);
-        }*/
     }
 }
--- src/server/YourServer.java
+++
@@ -1,4 +0,0 @@
-package server;
-
-public class YourServer extends BattleField{
-}
