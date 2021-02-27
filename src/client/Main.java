8e9f2ecc23975ecfbd4ee4bfd9f70022e88d8ff9
tandrecrosa14 <79652331+tandrecrosa14@users.noreply.github.com>
2021-02-27 17:21:58 +0000

    battlefield superclass added

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,12 +1,49 @@
 package client;
 
+import java.util.HashMap;
+import java.util.TreeMap;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        new Game();
+        /*String[][] axis = new String[11][11];
+
+        System.out.print(" ");
+        for(int i = 0; i < 11; i++)
+            System.out.print(i + " ");
+        System.out.println();
+
+        for(int i = 0; i < 11; i++) {
+            for(int j = 0; j < 11; j++) {
+                axis[i][j] = "";
+
+                if (j == 0) {
+                    System.out.print(i + "|");
+                } else if (j == axis.length - 1) {
+                    System.out.println(i);
+                }else {
+                    axis[i][j] = grid.get(alphabet.substring(j, j + 1) + i);
+                    System.out.print(" " + axis[i][j] + " ");
+                }
+            }
+        }
+         */
 
 
+       /* HashMap <String, String> grid = new HashMap<>();
 
+        String alphabet = "ABCDEFGHIJ";
+
+        for(int i = 0; i < 10; i++){
+            for (int j = 0; j < 10; j++) {
+
+                grid.put(alphabet.substring(i , i + 1) + j, "≈");
+
+            }
+        }
+        for (String j : grid.keySet()) {
+            System.out.println(j);
+        }*/
     }
 }
