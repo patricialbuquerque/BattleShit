1fb3e2e76512ce9152545cc08d3077ef840e47ec
carlosmfonseca4 <carlosmfonseca4@gmail.com>
2021-02-28 17:54:44 +0000

    Color Logic Added

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,5 +1,7 @@
 package client;
 
+import util.Symbols;
+
 import java.util.HashMap;
 import java.util.TreeMap;
 
@@ -11,17 +13,11 @@ public class Main {
         BattleField battleField = new BattleField();
         battleField.createField();
 
-        System.out.println(battleField.getGrid().get("A5").equals("≈"));
+        System.out.println(battleField.getGrid().get("A5").equals(Symbols.SEA));
 
         battleField.placeShipOnGrid("A3");
 
         battleField.showBattleField();
-
-
-
-
-
-
 
 
     }
--- src/util/Color.java
+++ src/util/Color.java
@@ -7,4 +7,14 @@ public class Color {
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_WHITE = "\u001B[0m";
 
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String COLOR1 = "\033[48;5;57m";
+    public static final String COLOR = "\033[48;5;159m";
+    public static final String RESET = "\033[0m";
+
+
+
+
 }
